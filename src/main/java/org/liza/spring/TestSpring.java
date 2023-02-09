7e9b47f43a0fdd98997bedfb609047e9f67bb0d6
alikeli <eli-com2@yandex.ru>
2023-02-09 19:43:44 +0100

    Init, Destroy methods

--- src/main/java/org/liza/spring/TestSpring.java
+++ src/main/java/org/liza/spring/TestSpring.java
@@ -6,21 +6,25 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext
                 ("applicationContext.xml");
+        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+        System.out.println(classicalMusic.getSong());
+
+
         //Music music = context.getBean("musicBean", Music.class);
         //MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
-        boolean comparison = musicPlayer1 == musicPlayer2;
-        System.out.println(comparison);
-
-        System.out.println(musicPlayer1);
-        System.out.println(musicPlayer2);
-
-        musicPlayer1.setVolume(10);
-
-        System.out.println(musicPlayer1.getVolume());
-        System.out.println(musicPlayer2.getVolume());
+//        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
+//        boolean comparison = musicPlayer1 == musicPlayer2;
+//        System.out.println(comparison);
+//
+//        System.out.println(musicPlayer1);
+//        System.out.println(musicPlayer2);
+//
+//        musicPlayer1.setVolume(10);
+//
+//        System.out.println(musicPlayer1.getVolume());
+//        System.out.println(musicPlayer2.getVolume());
 
 
 //        musicPlayer1.playMusic();
