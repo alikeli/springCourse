3a9831b8a6c6794e4d51470ca3272a22575f557c
alikeli <eli-com2@yandex.ru>
2023-02-10 10:04:30 +0100

    2 dependencies

--- src/main/java/org/liza/spring/MusicPlayer.java
+++ src/main/java/org/liza/spring/MusicPlayer.java
@@ -5,30 +5,15 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
-    @Autowired
-    private Music music;
-//    private String name;
-//    private int volume;
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public int getVolume() {
-//        return volume;
-//    }
-//
-//    public void setVolume(int volume) {
-//        this.volume = volume;
-//    }
+
+    private ClassicalMusic classicalMusic;
+    private RockMusic rockMusic;
 
     //IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
+    @Autowired
+    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
+        this.classicalMusic = classicalMusic;
+        this.rockMusic = rockMusic;
     }
 
     //    public MusicPlayer() {
@@ -40,7 +25,8 @@ public class MusicPlayer {
 //    }
 
     public void playMusic() {
-        System.out.println("Playing : " + music.getSong());
+        System.out.println("Playing : " + classicalMusic.getSong());
+        System.out.println("Playing : " + rockMusic.getSong());
     }
 
 }
